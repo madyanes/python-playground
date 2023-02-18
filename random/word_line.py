5ba567f17fd454e51608f68d74abc51603e75039
madyanes <madyanes.1995@gmail.com>
2023-02-18 20:39:55 +0700

    line can be added before the text

--- random/word_line.py
+++ random/word_line.py
@@ -1,16 +1,20 @@
 def get_line(amount :int) -> str:
     return '-' * amount
 
-def word_replacement(word :str, line_amount :int=10) -> str:
+def word_replacement(word :str, line_amount :int=10, right:bool=False) -> str:
     word_length:int = len(word)
     if word_length > line_amount:  # if the character is lesser than the line, raise an exception
         return False
     line_needed = line_amount - word_length
+    if right:
+        return get_line(line_needed) + word
     return word + get_line(line_needed)
 
 print(word_replacement('Hello, world?!', 100))
 print(word_replacement('Do you really like programming?', 100))
+print(word_replacement('Never stop learning!', 100, right=True))
 
 # output
 # Hello, world?!--------------------------------------------------------------------------------------
 # Do you really like programming?---------------------------------------------------------------------
+# --------------------------------------------------------------------------------Never stop learning!
